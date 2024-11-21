524a944c4a1d81d9a5b67412ddfb481dd5ea3a87
hyowonsong <woniwoni30@gmail.com>
2024-11-21 14:00:05 +0900

    Initial commit

---
+++ src/test/java/com/blogproject/api/BlogprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogproject.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
